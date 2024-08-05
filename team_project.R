bbb2ea6a99de49cb8bc4f6ccfa7a6b539af07f35
Yemam11 <1youssefemam@gmail.com>
2024-08-05 18:27:33 -0400

    Correlation testing for variable missingness

--- team_project.R
+++ team_project.R
@@ -24,6 +24,9 @@ eda <- function(sleep_data){
 #initialize libaries
 library(tidyverse)
 library(ggplot2)
+library(corrplot)
+library(Hmisc)
+library(mice)
 
 #import the data
 sleep_data <- read.csv("datasets/project_data.csv", header = T)
@@ -73,8 +76,34 @@ tab <- apply(ifelse(is.na(sleep_data), "Missing", "Not Missing"), 2, table)
 lapply(tab, prop.table)
 
 # PSS has 30% missingness, may make sense to exclude from the analysis
+sleep_data <- sleep_data %>% 
+  select(!Pittsburgh.Sleep.Quality.Index.Score)
+
+#See if we can identify a mechanism of missingness
+#convert NAs to true and non-NAs to false
+NA_matrix <- is.na(sleep_data)
+
+#calculate correlation matrix and p-value matrix
+cor_matrix <- Hmisc::rcorr(NA_matrix)
+
+diag(cor_matrix$r) <- 0
+diag(cor_matrix$P) <- 0
+
+cor_matrix$r[NA] <- 0
+cor_matrix$P[NA] <- 0
+
+
+#plot a heatmap
+# Note alot of NA values in rows where SD = 0, i.e all values are complete
+corrplot(cor_matrix$r, method = "square", p.mat = cor_matrix$P, insig = "blank", type = "lower", sig.level = 0.05, na.label = " ", tl.cex = 0.5)
+
+# it seems that alot of people who did not fill out one sleep quality/QOL score, also tended not to fill out the rest
 
 
 #Description of relevant data
 eda(sleep_data)
+
+#===============Imputation===================#
+# we need to do something to deal with the missing data
+
   
