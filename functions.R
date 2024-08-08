2a68e3dcc24faa2dcb27945586eb743dba72e73d
Yemam11 <1youssefemam@gmail.com>
2024-08-08 16:40:35 -0400

    Updated EDA figures

--- functions.R
+++ functions.R
@@ -45,19 +45,74 @@ generate_data <- function(model = "", response = "", predictor ="", data = imput
 
 #### EDA Function ####
 eda <- function(sleep_data){
-  #summarize mean age by gender
-  sleep_data.age <- sleep_data %>% 
-    group_by(Gender) %>%
-    summarise(mean_age = mean(Age, na.rm = T))
   
-  ggplot(data = sleep_data.age, mapping = aes(x = Gender, y= mean_age))+
-    geom_col()
+  sleep_data$Gender <- fct_recode(sleep_data$Gender, Male = "1", Female = "2")
   
-  #summarize BMI by gender
-  sleep_data.BMI <- sleep_data %>% 
-    group_by(Gender) %>%
-    summarise(mean_BMI = mean(BMI, na.rm = T))
+  #Plot age distribution
+  ggplot(data = sleep_data, mapping = aes(x = Age, fill = Gender))+
+    geom_histogram(col = "black")+
+    theme_minimal()+
+    labs(title = "Age Distribution", y = "Count")+
+    theme(plot.title = element_text(hjust =0.5))+
+    scale_fill_discrete(direction = -1)
   
-  ggplot(data = sleep_data.BMI, mapping = aes(x = Gender, y= mean_BMI))+
-    geom_col()
+  #plot BMI distribution
+  ggplot(data = summary_data, mapping = aes(x = BMI, fill = Gender))+
+    geom_histogram(col = "black")+
+    theme_minimal()+
+    labs(title = "BMI Distribution", y = "Count")+
+    theme(plot.title = element_text(hjust =0.5))+
+    scale_fill_discrete(direction = -1)
+  
+  #plot incidence of depression
+  ggplot(data = summary_data, mapping = aes(x = Depression, fill = Gender))+
+    geom_bar(col = "black", width = 0.3)+
+    theme_minimal()+
+    labs(title = "BMI Distribution", y = "Count")+
+    theme(plot.title = element_text(hjust =0.5))+
+    scale_fill_discrete(direction = -1)
+  
+  #plot ESS score distribution
+  ggplot(data = sleep_data, mapping = aes(x = Epworth.Sleepiness.Scale, fill = Gender))+
+    geom_histogram(col = "black")+
+    theme_minimal()+
+    labs(title = "Epworth Sleepiness Scale Score Distribution", y = "Count", x = "ESS Score")+
+    theme(plot.title = element_text(hjust =0.5))+
+    scale_fill_discrete(direction = -1)+
+    geom_vline(xintercept = 10, linetype = 2, size = 1)+
+    annotate(x=10,y=Inf,label="High Risk",vjust=1, hjust = 0.5, geom="label")
+  
+  #Plot BSS scores
+  ggplot(data = summary_data, mapping = aes(x = Berlin.Sleepiness.Scale, fill = Gender))+
+    geom_bar(col = "black", width = 0.3)+
+    theme_minimal()+
+    labs(title = "Berlin Sleepiness Scale Result Distribtion", y = "Count")+
+    theme(plot.title = element_text(hjust =0.5))+
+    scale_fill_discrete(direction = -1)
+  
+  #plot Athens insominia score distribution
+  ggplot(data = sleep_data, mapping = aes(x = Athens.Insomnia.Scale, fill = Gender))+
+    geom_histogram(col = "black")+
+    theme_minimal()+
+    labs(title = "Athens Insomnia Scale Score Distribution", y = "Count", x = "AIS Score")+
+    theme(plot.title = element_text(hjust =0.5))+
+    scale_fill_discrete(direction = -1)+
+    geom_vline(xintercept = 5.5, linetype = 2, size = 1)+
+    annotate(x=5.5,y=Inf,label="Insomina",vjust=1, hjust = -0.1, geom="label")
+  
+  #plot PCS
+  ggplot(data = sleep_data, mapping = aes(x = SF36.PCS, fill = Gender))+
+    geom_histogram(col = "black")+
+    theme_minimal()+
+    labs(title = "SF36 Physical Component Survery Score Distribution", y = "Count", x = "SF36 PCS Score")+
+    theme(plot.title = element_text(hjust =0.5))+
+    scale_fill_discrete(direction = -1)
+  
+  #plot PCS
+  ggplot(data = sleep_data, mapping = aes(x = SF36.MCS, fill = Gender))+
+    geom_histogram(col = "black")+
+    theme_minimal()+
+    labs(title = "SF36 Mental Component Survery Score Distribution", y = "Count", x = "SF36 MCS Score")+
+    theme(plot.title = element_text(hjust =0.5))+
+    scale_fill_discrete(direction = -1)
 }
